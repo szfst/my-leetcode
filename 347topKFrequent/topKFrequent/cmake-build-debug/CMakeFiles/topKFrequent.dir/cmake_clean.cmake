ceeb66820301b1b9aff6990bea42b4b6777d315d
fangshentian <jfdkajl>
2017-11-14 11:20:48 +0800

    refactor

---
+++ 347topKFrequent/topKFrequent/cmake-build-debug/CMakeFiles/topKFrequent.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/topKFrequent.dir/main.cpp.obj"
+  "topKFrequent.pdb"
+  "topKFrequent.exe"
+  "topKFrequent.exe.manifest"
+  "libtopKFrequent.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/topKFrequent.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
