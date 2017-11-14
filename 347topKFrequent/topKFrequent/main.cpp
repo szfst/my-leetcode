5268853cb354832bfda81ac2058095c071ba1bf8
fangshentian <jfdkajl>
2017-11-14 12:02:09 +0800

    other solution

--- 347topKFrequent/topKFrequent/main.cpp
+++ 347topKFrequent/topKFrequent/main.cpp
@@ -5,9 +5,10 @@
 
 using namespace std;
 
-//using priority queue to solve this problem with n log k time complexity
+
 class Solution {
 public:
+    //using priority queue to solve this problem with n log k time complexity
     vector<int> topKFrequent(vector<int>& nums, int k) {
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> q;
         vector<int> res;
@@ -23,7 +24,7 @@ public:
         for(int i = 0;i<len;i++){
             mp[nums[i]]++;
         }
-        for(unordered_map<int,int>::iterator itor = mp.begin();itor!=mp.end();itor++){
+        for(auto itor = mp.begin(); itor != mp.end(); itor++){
             if(q.size()==k){
                 if(itor->second>q.top().first){
                     q.pop();
@@ -45,9 +46,43 @@ public:
         return res;
 
     }
+    //using priority queue to solve this problem with n log n-k time complexity
+    vector<int> topKFrequent2(vector<int>& nums, int k) {
+        priority_queue<pair<int,int>> q;
+        vector<int> res;
+        unordered_map<int,int>  mp;
+        for (int num : nums) {
+            mp[num]++;
+        }
+        for (auto &itor : mp) {
+            if(q.size()==mp.size()-k){
+                if(itor.second<q.top().first){
+                    res.push_back(q.top().second);
+                    q.pop();
+                    //let the count of the number to be the first element,in order to make the queue compare the number
+                    q.push(make_pair(itor.second, itor.first));
+                }else{
+                    res.push_back(itor.first);
+                }
+            }else{
+                //let the count of the number to be the first element,in order to make the queue compare the number
+                q.push(make_pair(itor.second, itor.first));
+            }
+
+        }
+        return res;
+    }
 
 };
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int nums[] = {1, 1, 1, 2, 2, 3};
+    vector<int> vec( nums, nums + sizeof(nums)/sizeof(int));
+    int k = 2;
+
+    vector<int> res = Solution().topKFrequent2(vec, 2);
+    for( int i = 0 ; i < res.size() ; i ++ )
+        cout<<res[i]<<" ";
+    cout<<endl;
+
     return 0;
 }
