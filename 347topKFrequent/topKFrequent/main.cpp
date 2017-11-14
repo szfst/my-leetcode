983473c90ea1112223bf8565ea77cf36d149223d
fangshentian <jfdkajl>
2017-11-14 14:30:09 +0800

    add bucket sort solution

--- 347topKFrequent/topKFrequent/main.cpp
+++ 347topKFrequent/topKFrequent/main.cpp
@@ -56,7 +56,7 @@ public:
         }
         for (auto &itor : mp) {
             if(q.size()==mp.size()-k){
-                if(itor.second<q.top().first){
+                if(!q.empty() && itor.second<q.top().first){
                     res.push_back(q.top().second);
                     q.pop();
                     //let the count of the number to be the first element,in order to make the queue compare the number
@@ -72,14 +72,33 @@ public:
         }
         return res;
     }
+    //using bucket sort to solve this problem with n  time complexity
+    vector<int> topKFrequentBucketSort(vector<int>& nums, int k) {
+        priority_queue<pair<int,int>> q;
+        vector<int> res;
+        //frequency may equal to the size of the nums,so bucket size should be nums.size()+1;
+        vector<vector<int>> bucket(nums.size()+1);
+        unordered_map<int,int>  mp;
+        for (int num : nums) {
+            mp[num]++;
+        }
+        for (auto &iter : mp) {
+            int frequency = iter.second;
+            bucket[frequency].push_back(iter.first);
+        }
+        for(int pos = bucket.size()-1;res.size()<k;pos--){
+            res.insert(res.end(),bucket[pos].begin(),bucket[pos].end());
+        }
+        return res;
+    }
 
 };
 int main() {
-    int nums[] = {1, 1, 1, 2, 2, 3};
+    int nums[] = {1};
     vector<int> vec( nums, nums + sizeof(nums)/sizeof(int));
-    int k = 2;
+    int k = 1;
 
-    vector<int> res = Solution().topKFrequent2(vec, 2);
+    vector<int> res = Solution().topKFrequent2(vec,k);
     for( int i = 0 ; i < res.size() ; i ++ )
         cout<<res[i]<<" ";
     cout<<endl;
