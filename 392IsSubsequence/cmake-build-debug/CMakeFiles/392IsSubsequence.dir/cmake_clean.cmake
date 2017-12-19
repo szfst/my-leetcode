10d28438059986edb7bccc21becdaf8b7edbd75a
fangshentian <jfdkajl>
2017-12-19 12:21:36 +0800

    add solution

---
+++ 392IsSubsequence/cmake-build-debug/CMakeFiles/392IsSubsequence.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/392IsSubsequence.dir/main.cpp.obj"
+  "392IsSubsequence.pdb"
+  "392IsSubsequence.exe"
+  "392IsSubsequence.exe.manifest"
+  "lib392IsSubsequence.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/392IsSubsequence.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
